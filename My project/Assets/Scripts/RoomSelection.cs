ca18427b4466f6c78d3774120b25a9e8972a9465
Sai <Sai2.Non@live.uwe.ac.uk>
2025-03-20 20:14:04 +0000

    more room added

--- My project/Assets/Scripts/RoomSelection.cs
+++ My project/Assets/Scripts/RoomSelection.cs
@@ -13,7 +13,7 @@ public class RoomSelection : MonoBehaviour
 
     private List<string> roomCodes = new List<string>
     {
-        "2Q048", "2Q049", "2Q050", "2Q047", "2Q052"
+        "2Q048", "2Q049", "2Q050", "2Q047", "2Q052", "2Q046", "2Q043", "2Q042", "2Q041","2Q044A","2Q044B","2Q054","2Q053","2Q012","2Q028","2Q045","2Q035","2Q034","2Q033","2Q056","2Q056A","2Q056B","2Q056C","2Q032","2Q055", "2Q002","2Q001A","2Q008","2Q030","2Q006A","2Q029",
     };
     // Start is called before the first frame update
     void Start()
